df782ff19b11eb7e22b9359193a389f9efbdb617
Norbert <54719382+NorbertCode@users.noreply.github.com>
2025-02-17 19:46:32 +0100

    plan data is now read from json

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,8 @@ import DateSwitch from "./dateSwitch.js";
 import { formatDate } from "./utils.js";
 import TimeColumn from "./timeColumn.js";
 
+import plan from "./planData.json";
+
 const date = new Date(Date.now()); // Holds the date as a modifiable object
 let weekday = date.getDay();
 
@@ -14,36 +16,6 @@ export default function Page() {
     const height = 600;
     const timeStart = 800;
     const timeEnd = 2000;
-    const plan = [
-        {
-            day: 1,
-            classes: [
-                {
-                    name: "A",
-                    timeStart: 1130,
-                    timeEnd: 1315,
-                    dateStart: "17.2.2025",
-                    dateEnd: "17.3.2025",
-                    type: "Wykład",
-                    place: "s. 123"
-                }
-            ]
-        },
-        {
-            day: 2,
-            classes: [
-                {
-                    name: "B",
-                    timeStart: 1500,
-                    timeEnd: 1600,
-                    dateStart: "17.1.2025",
-                    dateEnd: "17.2.2025",
-                    type: "Ćwiczenia",
-                    place: "s. 456"
-                }
-            ]
-        }
-    ];
 
     const [dateStr, setDate] = useState(formatDate(date));
     function moveDate(increment) {
