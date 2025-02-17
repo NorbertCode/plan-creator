fb4d070abfedf3d121cd9eb58af01f92468f9a92
Norbert <54719382+NorbertCode@users.noreply.github.com>
2025-02-17 17:27:44 +0100

    fixed timeToStr

--- src/app/utils.js
+++ src/app/utils.js
@@ -19,6 +19,14 @@ export function isDateBetween(currentDateStr, startDateStr, endDateStr) {
 // --- Time functions ---
 export function timeToFloat(time) {
     const hours = Math.ceil(time / 100);
-    const seconds = (time % 100) / 60
-    return hours + seconds
+    const minutes = (time % 100) / 60
+    return hours + minutes
+}
+
+export function timeToStr(time) {
+    const hours = Math.floor(time / 100).toString();
+    let minutes = (time % 100).toString();
+    if (minutes == "0")
+        minutes = "00";
+    return hours + ":" + minutes;
 }
