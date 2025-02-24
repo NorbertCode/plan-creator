5d6ff40695b87b44f9d36f8a189705935efef67c
Norbert <54719382+NorbertCode@users.noreply.github.com>
2025-02-24 01:14:40 +0100

    made window stuff client-side

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,12 @@
 'use client'
-import { useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 
 import DateSwitch from "./dateSwitch.js";
 import ContentTable from "./contentTable.js";
 import { getFutureDate } from "./utils.js";
 
-
 export default function Page() {
-    const [mode, setMode] = useState(window.innerHeight > window.innerWidth ? "single" : "multiple");
+    const [mode, setMode] = useState(typeof window === "undefined" || window.innerHeight > window.innerWidth ? "single" : "multiple");
     let dateIncrement = mode == "single" ? 1 : 7;
     function refreshMode() {
         if (window.innerHeight > window.innerWidth) {
@@ -19,7 +18,9 @@ export default function Page() {
             dateIncrement = 7;
         }
     }
-    window.addEventListener("resize", refreshMode);
+    useEffect(() => {
+        window.addEventListener("resize", refreshMode);
+    });
 
     const [date, setDate] = useState(new Date(Date.now()));
     function moveDate(increment) {
