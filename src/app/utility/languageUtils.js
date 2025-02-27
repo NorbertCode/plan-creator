da90a6ecb6272c81c58df88f44e77c5077409165
Norbert <54719382+NorbertCode@users.noreply.github.com>
2025-02-27 23:14:52 +0100

    added fallback language

--- src/app/utility/languageUtils.js
+++ src/app/utility/languageUtils.js
@@ -1,7 +1,11 @@
 import languageConfig from '../config/languageConfig';
 
 const language = navigator.language.slice(0, 2);
+const fallbackLanguage = "en";
 
 export function getText(key) {
+    if (languageConfig[language] === undefined) {
+        return languageConfig[fallbackLanguage][key];
+    }
     return languageConfig[language][key];
 }
